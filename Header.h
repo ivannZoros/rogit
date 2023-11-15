3764243096c75740de8bac0cdd020b9920acd6fa
Ivan <146617492+ivannZoros@users.noreply.github.com>
2023-11-15 20:56:21 +0600

    maps + lvls !!! without end of game

--- Header.h
+++ Header.h
@@ -8,7 +8,7 @@
 using namespace std;
 
 static bool isGameover = false;
-
+int lvl = 0;
 int heroX = 1;
 int heroY = 1;
 int lives = 10;
@@ -22,6 +22,24 @@ char  map[7][8] = {
         {'#','.','$','.','.','.','>','#'},
         {'#','#','#','#','#','#','#','#'}
 };
+char  map1[7][8] = {
+        {'#','#','#','#','#','#','#','#'},
+        {'#','.','.','.','.','.','.','#'},
+        {'#','.','.','.','.','$','.','#'},
+        {'#','.','$','.','.','.','.','#'},
+        {'#','.','.','.','.','.','.','#'},
+        {'#','.','.','.','.','.','>','#'},
+        {'#','#','#','#','#','#','#','#'}
+};
+char  map2[7][8] = {
+        {'#','#','#','#','#','#','#','#'},
+        {'#','.','.','.','.','.','.','#'},
+        {'#','.','.','.','.','.','.','#'},
+        {'#','.','.','.','.','$','.','#'},
+        {'#','.','.','.','.','.','.','#'},
+        {'#','$','.','.','.','.','>','#'},
+        {'#','#','#','#','#','#','#','#'}
+};
 char PrintMap()
 {
 
@@ -29,7 +47,7 @@ char PrintMap()
     {
         for (int j = 0; j < 8; j++) {
 
-            if (i == heroY && j == heroX) {
+            if (i == heroY && j == heroX && lvl == 0) {
                 cout << " " << "@";
             }
             else {
@@ -41,9 +59,51 @@ char PrintMap()
 
     }
     cout << "Max lives - " << lives << endl;
-    if(map[heroY][heroX] == '>'){
-        cout << "dsitk";
+    cout << "level 1"  << endl;
+    return 0;
+}
+char PrintMap1()
+{
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 8; j++) {
+
+            if (i == heroY && j == heroX && lvl == 1) {
+                cout << " " << "@";
+            }
+            else {
+                cout << " " << map1[i][j];
+            }
+        }
+
+        cout << endl;
+
     }
+    cout << "Max lives - " << lives << endl;
+    cout << "level 2"  << endl;
+    return 0;
+}
+char PrintMap2()
+{
+
+    for (int i = 0; i < 7; i++)
+    {
+        for (int j = 0; j < 8; j++) {
+
+            if (i == heroY && j == heroX && lvl == 2) {
+                cout << " " << "@";
+            }
+            else {
+                cout << " " << map2[i][j];
+            }
+        }
+
+        cout << endl;
+
+    }
+    cout << "Max lives - " << lives << endl;
+    cout << "level 3"  << endl;
+
     return 0;
 }
 
--- main.cpp
+++ main.cpp
@@ -3,15 +3,23 @@ int main()
 {
 
     while (!isGameover) {
-        PrintMap();
+
+        if (lvl == 0) {
+            PrintMap();
+        }
+        if (lvl == 1){
+            PrintMap1();
+        }
+        if (lvl == 2){
+            PrintMap2();
+        }
         int i_input;
         i_input = _getch();
-
         switch (i_input)
         {
             case 72:
             {
-                if (heroY - 1 >= 0 && map[heroY - 1][heroX] != '#')
+                if (heroY - 1 >= 0 && (map[heroY - 1][heroX] != '#' || map1[heroY - 1][heroX] != '#' || map2[heroY - 1][heroX]  != '#'))
                 {
                     heroY--;
                     --lives;
@@ -20,7 +28,7 @@ int main()
             }
             case 80:
             {
-                if (heroY + 1 < 7 && map[heroY + 1][heroX] != '#')
+                if (heroY + 1 < 7 && (map[heroY + 1][heroX] != '#' || map1[heroY + 1][heroX] != '#' || map2[heroY + 1][heroX] != '#'))
                 {
                     heroY++;
                     --lives;
@@ -29,7 +37,7 @@ int main()
             }
             case 75:
             {
-                if (heroX - 1 >= 0 && map[heroY][heroX - 1] != '#')
+                if (heroX - 1 >= 0 && (map[heroY][heroX - 1] != '#' || map1[heroY][heroX - 1] != '#' || map2[heroY][heroX - 1] != '#' ))
                 {
                     heroX--;
                     --lives;
@@ -38,7 +46,7 @@ int main()
             }
             case 77:
             {
-                if (heroX + 1 < 8 && map[heroY][heroX + 1] != '#')
+                if (heroX + 1 < 8 && (map[heroY][heroX + 1] != '#' || map1[heroY][heroX + 1] != '#' || map2[heroY][heroX + 1] != '#'))
                 {
                     heroX++;
                     --lives;
@@ -47,12 +55,28 @@ int main()
             }
 
         }
-        if (map[heroY][heroX] == '$')
+        if (map[heroY][heroX] == '$' && lvl == 0 )
         {
             map[heroY][heroX] = '.';
             lives += 3;
         }
+        if (map1[heroY][heroX] == '$' && lvl == 1 )
+        {
+            map1[heroY][heroX] = '.';
+            lives += 3;
+        }
+        if (map2[heroY][heroX] == '$' && lvl == 2 )
+        {
+            map2[heroY][heroX] = '.';
+            lives += 3;
+        }
+        if (map[heroY][heroX] == '>' || map1[heroY][heroX] == '>' || map2[heroY][heroX] == '>' )
+        {
+            lvl += 1;
+            heroX = 1;
+            heroY = 1;
 
+        }
         if (lives == -1) {
             char choose;
             cout << "GAME OVER\n";
@@ -61,22 +85,18 @@ int main()
             cin >> choose;
             if (choose == 'y') {
                 isGameover = false;
-
                 lives = 10;
             }
             else if (choose == 'n') {
                 break;
             }
 
-
-
         }
-
-
-
+        if (map2[heroY][heroX] == '>' && lvl == 2){
+            cout << "You are WIN!!!"<< endl;
+        }
         system("cls");
     }
-
 
     return 0;
 }
